0e8bf6d70c785c30822bc68005c17e89e6318e65
whuu <whuu@gazeta.pl>
2019-10-10 16:08:49 +0000

    initial structure

---
+++ src/Foundation/FormsCustomScripts/code/Models/CustomScriptButtonViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+
+namespace Sc.FormsCustomScripts.Models
+{
+    public class CustomScriptButtonViewModel : ButtonViewModel
+    {
+        public string FormSubmitScript { get; set; }
+
+        protected override void InitItemProperties(Item item)
+        {
+            Assert.ArgumentNotNull(item, nameof(item));
+            base.InitItemProperties(item);
+            FormSubmitScript = StringUtil.GetString((object)item.Fields["Form Submit Script"]);
+        }
+
+        protected override void UpdateItemFields(Item item)
+        {
+            Assert.ArgumentNotNull(item, nameof(item));
+            base.UpdateItemFields(item);
+            item.Fields["Form Submit Script"]?.SetValue(FormSubmitScript, false);
+        }
+    }
+}
